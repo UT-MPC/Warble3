28dc51e241f5fe860620d78bc7e3d3c45e073f75
yosefsaputra <yosef.saputra@gmail.com>
2018-06-29 23:09:29 -0500

    To add RecyclerView on ParentBridgeActivity. To add EditText into SerialNumberActivity.

--- app/src/main/java/edu/utexas/mpc/warble3/frontend/setup_page/ParentBridgeActivity.java
+++ app/src/main/java/edu/utexas/mpc/warble3/frontend/setup_page/ParentBridgeActivity.java
@@ -2,12 +2,20 @@ package edu.utexas.mpc.warble3.frontend.setup_page;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 import edu.utexas.mpc.warble3.R;
+import edu.utexas.mpc.warble3.frontend.adapter.SimpleAdapter;
 
 public class ParentBridgeActivity extends AppCompatActivity {
+    RecyclerView parentBridgeRecycleView;
+    RecyclerView.LayoutManager parentBridgeLayoutManager;
+    RecyclerView.Adapter parentBridgeAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,5 +24,17 @@ public class ParentBridgeActivity extends AppCompatActivity {
 
         Objects.requireNonNull(getSupportActionBar()).setTitle(R.string.titleParentBridge_addLightPage);
 
+        // TODO: Change to list of light types
+        List<String> parentBridges = new ArrayList<>();
+        parentBridges.add("Bridge 1");
+        parentBridges.add("Bridge 2");
+        parentBridges.add("Bridge 3");
+        parentBridges.add("Bridge 4");
+
+        parentBridgeRecycleView = findViewById(R.id.parentBridge_recyclerView);
+        parentBridgeLayoutManager = new LinearLayoutManager(this);
+        parentBridgeRecycleView.setLayoutManager(parentBridgeLayoutManager);
+        parentBridgeAdapter = new SimpleAdapter(this, parentBridges);
+        parentBridgeRecycleView.setAdapter(parentBridgeAdapter);
     }
 }
