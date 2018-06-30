9f570edb0fe2bec634a9736e9f3bd7158a013cd9
yosefsaputra <yosef.saputra@gmail.com>
2018-06-29 21:05:22 -0500

    To add activity and adapter for BridgeType selection

---
+++ app/src/main/java/edu/utexas/mpc/warble3/frontend/adapter/SimpleAdapter.java
@@ -0,0 +1,50 @@
+package edu.utexas.mpc.warble3.frontend.adapter;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.List;
+
+import edu.utexas.mpc.warble3.R;
+
+public class SimpleAdapter extends RecyclerView.Adapter<SimpleAdapter.ViewHolder> {
+
+    private final Context context;
+    private final List<String> dataset;
+
+    public SimpleAdapter(Context context, List<String> dataset) {
+        this.context = context;
+        this.dataset = dataset;
+    }
+
+    @NonNull
+    @Override
+    public SimpleAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View itemView = LayoutInflater.from(parent.getContext()).inflate(R.layout.layout_simple_recycler_view, parent, false);
+        return new ViewHolder(itemView);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull SimpleAdapter.ViewHolder holder, int position) {
+        holder.nameTextView.setText(dataset.get(position));
+    }
+
+    @Override
+    public int getItemCount() {
+        return dataset.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        public TextView nameTextView;
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+            nameTextView = itemView.findViewById(R.id.name_textView);
+        }
+    }
+}
--- app/src/main/java/edu/utexas/mpc/warble3/frontend/setup_page/BridgeTypeActivity.java
+++ app/src/main/java/edu/utexas/mpc/warble3/frontend/setup_page/BridgeTypeActivity.java
@@ -2,12 +2,20 @@ package edu.utexas.mpc.warble3.frontend.setup_page;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 import edu.utexas.mpc.warble3.R;
+import edu.utexas.mpc.warble3.frontend.adapter.SimpleAdapter;
 
 public class BridgeTypeActivity extends AppCompatActivity {
+    private RecyclerView bridgeTypeRecyclerView;
+    private RecyclerView.LayoutManager bridgeTypeLayoutManager;
+    private RecyclerView.Adapter bridgeTypeAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,5 +24,18 @@ public class BridgeTypeActivity extends AppCompatActivity {
 
         Objects.requireNonNull(getSupportActionBar()).setTitle(R.string.titleBridgeType_addBridgePage);
 
+        bridgeTypeRecyclerView = findViewById(R.id.bridgeType_recyclerView);
+        bridgeTypeRecyclerView.setHasFixedSize(false);
+
+        bridgeTypeLayoutManager = new LinearLayoutManager(this);
+        bridgeTypeRecyclerView.setLayoutManager(bridgeTypeLayoutManager);
+
+        // TODO: Change to list of bridge types
+        List<String> bridgeTypes = new ArrayList<>();
+        bridgeTypes.add("Philips Hub");
+        bridgeTypes.add("Wink Hub");
+
+        bridgeTypeAdapter = new SimpleAdapter(getApplicationContext(), bridgeTypes);
+        bridgeTypeRecyclerView.setAdapter(bridgeTypeAdapter);
     }
 }
