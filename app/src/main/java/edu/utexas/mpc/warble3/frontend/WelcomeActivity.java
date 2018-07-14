6190efcc2e24195820dd033946faaa124498a834
yosefsaputra <yosef.saputra@gmail.com>
2018-07-13 20:32:03 -0500

    To add temporary sign in method, basically to give username only

--- app/src/main/java/edu/utexas/mpc/warble3/frontend/WelcomeActivity.java
+++ app/src/main/java/edu/utexas/mpc/warble3/frontend/WelcomeActivity.java
@@ -1,19 +1,60 @@
 package edu.utexas.mpc.warble3.frontend;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 import edu.utexas.mpc.warble3.R;
 
 public class WelcomeActivity extends AppCompatActivity {
+    public static String SHARED_PREFERENCES_CURRENT_USER_SETTINGS = "edu.utexas.mpc.warble3.CURRENT_USER_SETTINGS";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_welcome);
 
-        Button gobutton1 = findViewById(R.id.usernameSend_welcomePage_button);
-        Button gobutton2 = findViewById(R.id.newUsernameSend_welcomePage_button);
+        SharedPreferences sharedPrefs = getSharedPreferences(SHARED_PREFERENCES_CURRENT_USER_SETTINGS, MODE_PRIVATE);
+        if (sharedPrefs.getString("username", null) != null) {
+            Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+            startActivity(intent);
+        }
+        else {
+            setContentView(R.layout.activity_welcome);
+
+            final EditText username_editText = findViewById(R.id.username_welcomePage_editText);
+            final EditText new_username_editText = findViewById(R.id.newUsername_welcomePage_editText);
+
+            View.OnClickListener onClickListener1 = new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    SharedPreferences.Editor editor = getSharedPreferences(SHARED_PREFERENCES_CURRENT_USER_SETTINGS, MODE_PRIVATE).edit();
+                    editor.putString("username", username_editText.getText().toString());
+                    editor.apply();
+
+                    Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                    startActivity(intent);
+                }
+            };
+            Button gobutton1 = findViewById(R.id.usernameSend_welcomePage_button);
+            gobutton1.setOnClickListener(onClickListener1);
+
+            View.OnClickListener onClickListener2 = new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    SharedPreferences.Editor editor = getSharedPreferences(SHARED_PREFERENCES_CURRENT_USER_SETTINGS, MODE_PRIVATE).edit();
+                    editor.putString("username", new_username_editText.getText().toString());
+                    editor.apply();
+
+                    Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                    startActivity(intent);
+                }
+            };
+            Button gobutton2 = findViewById(R.id.newUsernameSend_welcomePage_button);
+            gobutton2.setOnClickListener(onClickListener2);
+        }
     }
 }
