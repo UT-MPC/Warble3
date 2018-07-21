24424eb29dd2f84d89f7735310aad9a9e14b83df
yosefsaputra <yosef.saputra@gmail.com>
2018-07-20 19:07:03 -0500

    To add users and things in resource. To initialize resource during app oncreate.

--- app/src/main/java/edu/utexas/mpc/warble3/model/resource/Resource.java
+++ app/src/main/java/edu/utexas/mpc/warble3/model/resource/Resource.java
@@ -1,5 +1,45 @@
 package edu.utexas.mpc.warble3.model.resource;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import edu.utexas.mpc.warble3.model.thing.component.Thing;
+import edu.utexas.mpc.warble3.model.user.User;
+
 public class Resource {
     private static final String TAG = "Resource";
+
+    private static Resource instance;
+
+    private List<User> users = new ArrayList<>();
+    private List<Thing> things = new ArrayList<>();
+
+    private Resource() {}
+
+    public static Resource getInstance() {
+        if (instance == null) {
+            instance = new Resource();
+        }
+        return instance;
+    }
+
+    public List<User> getUsers() {
+        return users;
+    }
+
+    public void addUser(User newUser) {
+        // TODO: implement
+    }
+
+    public void addUsers(List<User> newUsers) {
+        // TODO: implement
+    }
+
+    public List<Thing> getThings() {
+        return things;
+    }
+
+    public void addThings(List<Thing> newThings) {
+        // TODO: implement
+    }
 }
