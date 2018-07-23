1f2a81ec7359f3fc2bf190eb88a42307febcdba9
yosefsaputra <yosef.saputra@gmail.com>
2018-07-23 14:30:12 -0500

    To add ConnectionConverter skeleton

---
+++ app/src/main/java/edu/utexas/mpc/warble3/database/converter/ConnectionConverter.java
@@ -0,0 +1,17 @@
+package edu.utexas.mpc.warble3.database.converter;
+
+import edu.utexas.mpc.warble3.database.ConnectionDb;
+import edu.utexas.mpc.warble3.model.thing.connect.Connection;
+
+// TODO: implement
+public class ConnectionConverter {
+    public static Connection toConnection(ConnectionDb connectionDb) {
+        // TODO: implement
+        return null;
+    }
+
+    public static ConnectionDb toConnectionDb(Connection connection) {
+        // TODO: implement
+        return null;
+    }
+}
