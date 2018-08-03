491d4d9f0b4d18676ce2e75b412091f096945701
yosefsaputra <yosef.saputra@gmail.com>
2018-08-03 12:11:34 -0500

    To set the standard log level to INFO

--- app/src/main/java/edu/utexas/mpc/warble3/util/Logging.java
+++ app/src/main/java/edu/utexas/mpc/warble3/util/Logging.java
@@ -1,10 +1,11 @@
 package edu.utexas.mpc.warble3.util;
 
 public class Logging {
-    public static int LEVEL = 5;
+    public static int LEVEL = 4;
     public static boolean ERROR = LEVEL > 0;
     public static boolean WARN = LEVEL > 1;
     public static boolean INFO = LEVEL > 2;
+
     public static boolean DEBUG = LEVEL > 3;
     public static boolean VERBOSE = LEVEL > 4;
 }
