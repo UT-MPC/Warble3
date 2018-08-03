0d7c53a9c7363e832b6c1b05fd6436f17b092120
yosefsaputra <yosef.saputra@gmail.com>
2018-08-03 16:32:42 -0500

    To override toString of Discovery and PhilipsHueUPnPDiscovery.

--- app/src/main/java/edu/utexas/mpc/warble3/model/thing/discovery/Discovery.java
+++ app/src/main/java/edu/utexas/mpc/warble3/model/thing/discovery/Discovery.java
@@ -6,6 +6,15 @@ import java.util.List;
 import edu.utexas.mpc.warble3.model.thing.component.Thing;
 
 public abstract class Discovery implements Serializable {
+    private static String TAG = "Discovery";
+
     public abstract List<? extends Thing> onDiscover();
     public abstract List<? extends Thing> onDiscoverDescendants();
+
+    @Override
+    public String toString() {
+        String string = "";
+        string += String.format("%s", TAG);
+        return string;
+    }
 }
