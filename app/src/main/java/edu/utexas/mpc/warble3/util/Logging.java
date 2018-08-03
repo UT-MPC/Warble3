310aad5446129d354e51038780a4440a3046c102
yosefsaputra <yosef.saputra@gmail.com>
2018-08-03 12:12:00 -0500

    To remove excessive new line

--- app/src/main/java/edu/utexas/mpc/warble3/util/Logging.java
+++ app/src/main/java/edu/utexas/mpc/warble3/util/Logging.java
@@ -5,7 +5,6 @@ public class Logging {
     public static boolean ERROR = LEVEL > 0;
     public static boolean WARN = LEVEL > 1;
     public static boolean INFO = LEVEL > 2;
-
     public static boolean DEBUG = LEVEL > 3;
     public static boolean VERBOSE = LEVEL > 4;
 }
