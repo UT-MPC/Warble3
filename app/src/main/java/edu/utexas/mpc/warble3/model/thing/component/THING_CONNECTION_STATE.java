e22987c2ee755354cffe26987a138ac6e1c8ecb7
yosefsaputra <yosef.saputra@gmail.com>
2018-08-04 10:43:56 -0500

    To introduce THING_CONNECTION_STATE

---
+++ app/src/main/java/edu/utexas/mpc/warble3/model/thing/component/THING_CONNECTION_STATE.java
@@ -0,0 +1,11 @@
+package edu.utexas.mpc.warble3.model.thing.component;
+
+public enum THING_CONNECTION_STATE {
+    INITIAL,
+    UNREACHABLE,
+    DISCOVERED,
+    UNAUTHENTICATED,
+    AUTHENTICATED,
+    BOUNDED,
+    UNKNOWN
+}
