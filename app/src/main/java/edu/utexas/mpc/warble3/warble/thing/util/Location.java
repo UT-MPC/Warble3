961785ceb3d8fc34c82439f10857951fe4c7fa73
yosefsaputra <yosef.saputra@gmail.com>
2018-09-03 16:35:48 -0500

    To define Location class

---
+++ app/src/main/java/edu/utexas/mpc/warble3/warble/thing/util/Location.java
@@ -0,0 +1,78 @@
+package edu.utexas.mpc.warble3.warble.thing.util;
+
+import com.google.gson.Gson;
+import com.google.gson.annotations.Expose;
+
+public class Location {
+    @Expose private double latitude;
+    @Expose private double longitude;
+
+    @Expose private double indoorX;
+    @Expose private double indoorY;
+    @Expose private double indoorZ;
+
+    public Location() {}
+
+    public Location(double latitude, double longitude, double indoorX, double indoorY, double indoorZ) {
+        this.latitude = latitude;
+        this.longitude = longitude;
+        this.indoorX = indoorX;
+        this.indoorY = indoorY;
+        this.indoorZ = indoorZ;
+    }
+
+    public double getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(double latitude) {
+        this.latitude = latitude;
+    }
+
+    public double getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(double longitude) {
+        this.longitude = longitude;
+    }
+
+    public double getIndoorX() {
+        return indoorX;
+    }
+
+    public void setIndoorX(double indoorX) {
+        this.indoorX = indoorX;
+    }
+
+    public double getIndoorY() {
+        return indoorY;
+    }
+
+    public void setIndoorY(double indoorY) {
+        this.indoorY = indoorY;
+    }
+
+    public double getIndoorZ() {
+        return indoorZ;
+    }
+
+    public void setIndoorZ(double indoorZ) {
+        this.indoorZ = indoorZ;
+    }
+
+    @Override
+    public String toString() {
+        return "Location{" +
+                "latitude=" + latitude +
+                ", longitude=" + longitude +
+                ", indoorX=" + indoorX +
+                ", indoorY=" + indoorY +
+                ", indoorZ=" + indoorZ +
+                '}';
+    }
+
+    public static Location fromString(String string) {
+        return new Gson().fromJson(string.replace("Location", ""), Location.class);
+    }
+}
