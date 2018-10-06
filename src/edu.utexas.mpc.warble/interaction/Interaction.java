a89d6f89c58de24425b96f310f9621d2dd12ae85
yosefsaputra <yosef.saputra@gmail.com>
2018-10-05 22:39:25 -0500

    To create Interaction class

---
+++ src/edu.utexas.mpc.warble/interaction/Interaction.java
@@ -0,0 +1,89 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2018 Yosef Saputra
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package edu.utexas.mpc.warble.interaction;
+
+import edu.utexas.mpc.warble.thing.util.Location;
+
+import java.util.logging.Logger;
+
+public class Interaction {
+    private static final String TAG = Interaction.class.getSimpleName();
+    private static final Logger LOGGER = Logger.getLogger(TAG);
+
+    private long timestamp;
+    private Location location;
+
+    private String username;
+    private String command;
+    private String thingUuid;
+
+    public Interaction(Location location, String username, String command, String thingUuid) {
+        this.location = location;
+        this.username = username;
+        this.command = command;
+        this.thingUuid = thingUuid;
+        this.timestamp = System.currentTimeMillis();
+    }
+
+    public long getTimestamp() {
+        return timestamp;
+    }
+
+    public void resetTimestamp() {
+        timestamp = System.currentTimeMillis();
+    }
+
+    public Location getLocation() {
+        return location;
+    }
+
+    public void setLocation(Location location) {
+        this.location = location;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getCommand() {
+        return command;
+    }
+
+    public void setCommand(String command) {
+        this.command = command;
+    }
+
+    public String getThingUuid() {
+        return thingUuid;
+    }
+
+    public void setThingUuid(String thingUuid) {
+        this.thingUuid = thingUuid;
+    }
+}
