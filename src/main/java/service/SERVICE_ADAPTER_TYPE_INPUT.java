7700a02d70d2de1dfb5bbe9d2c1c5e95e2f15284
yosefsaputra <yosef.saputra@gmail.com>
2018-10-29 02:49:43 -0500

    To split service adapter type enum to input and output

---
+++ src/main/java/service/SERVICE_ADAPTER_TYPE_INPUT.java
@@ -0,0 +1,8 @@
+package service;
+
+public enum SERVICE_ADAPTER_TYPE_INPUT {
+    // Context Builder
+    TIME,
+    LOCATION,
+    SENSOR,
+}
---
+++ src/main/java/service/SERVICE_ADAPTER_TYPE_OUTPUT.java
@@ -0,0 +1,10 @@
+package service;
+
+public enum SERVICE_ADAPTER_TYPE_OUTPUT {
+    // Communication Service
+    HTTP,
+    BLUETOOTH,
+
+    // Database
+    DATABASE,
+}
