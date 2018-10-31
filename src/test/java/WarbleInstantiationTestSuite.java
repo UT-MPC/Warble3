29d49c97d8662f1877a67489e80023321c1bdad8
yosefsaputra <yosef.saputra@gmail.com>
2018-10-30 21:49:05 -0500

    To check the equality of getServiceAdapter() and setServiceAdapter()

--- src/test/java/WarbleInstantiationTestSuite.java
+++ src/test/java/WarbleInstantiationTestSuite.java
@@ -2,6 +2,8 @@ import org.junit.Test;
 import service.SERVICE_ADAPTER_TYPE_INPUT;
 import service.SERVICE_ADAPTER_TYPE_OUTPUT;
 
+import static org.junit.Assert.assertEquals;
+
 public class WarbleInstantiationTestSuite {
     @Test
     public void WarbleInstantiation() {
@@ -12,8 +14,16 @@ public class WarbleInstantiationTestSuite {
     public void WarbleServiceAdapterInitialization() {
         Warble warble = new Warble();
 
-        warble.setServiceAdapter(SERVICE_ADAPTER_TYPE_OUTPUT.DATABASE, new MockDatabaseServiceAdapter());
-        warble.setServiceAdapter(SERVICE_ADAPTER_TYPE_INPUT.LOCATION, new MockLocationServiceAdapter());
-        warble.setServiceAdapter(SERVICE_ADAPTER_TYPE_OUTPUT.HTTP, new MockHttpServiceAdapter());
+        MockDatabaseServiceAdapter mockDatabaseServiceAdapter = new MockDatabaseServiceAdapter();
+        MockLocationServiceAdapter mockLocationServiceAdapter = new MockLocationServiceAdapter();
+        MockHttpServiceAdapter mockHttpServiceAdapter = new MockHttpServiceAdapter();
+
+        warble.setServiceAdapter(SERVICE_ADAPTER_TYPE_OUTPUT.DATABASE, mockDatabaseServiceAdapter);
+        warble.setServiceAdapter(SERVICE_ADAPTER_TYPE_INPUT.LOCATION, mockLocationServiceAdapter);
+        warble.setServiceAdapter(SERVICE_ADAPTER_TYPE_OUTPUT.HTTP, mockHttpServiceAdapter);
+
+        assertEquals(mockDatabaseServiceAdapter, warble.getServiceAdapter(SERVICE_ADAPTER_TYPE_OUTPUT.DATABASE));
+        assertEquals(mockLocationServiceAdapter, warble.getServiceAdapter(SERVICE_ADAPTER_TYPE_INPUT.LOCATION));
+        assertEquals(mockHttpServiceAdapter, warble.getServiceAdapter(SERVICE_ADAPTER_TYPE_OUTPUT.HTTP));
     }
 }
